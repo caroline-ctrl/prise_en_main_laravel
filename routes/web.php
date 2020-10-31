c17606625348be77a9a4732a30b50f4a686252c7
caroline-ctrl <duon.caroline@gmail.com>
2020-10-31 21:50:07 +0100

    création d'une migration

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 // la route prend en parametre un chiffre compris entre 1 et 3
 // laravel met a disposition la fonction request() qui recupère le chiffre fourni par l'utilisateur a traver l'url
 Route::get('{n}', function () {
     return 'je suis la page ' . request('n');
 })->where('n', '[1, 2, 3]');
 
+
+
 // route qui recupère le prenom renseigné par l'utilisateur
 // l'url ressemblera a : http://localhost:8000/bonjour/oxie
 Route::get('/salut/{prenom}', function() {
     return 'Salut ' . request('prenom');
 });
+
+
 
 // pour garder la vue propre et organisé, il faut éviter de faire des appels de fonction dans la vue
 Route::get('/bonjour/{name_url}', function () {
@@ -40,10 +46,14 @@ Route::get('/bonjour/{name_url}', function () {
     ]);
 });
 
+
+
 Route::get('/inscription', function () {
     return view('inscription');
 });
 
+
+
 Route::post('/inscription', function () {
-    return 'formulaire recu';
+    return 'votre email est ' . request('email');
 });;
