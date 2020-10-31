11072010ae7f370694f8a2bb7ea43e8cdb97ca16
caroline-ctrl <duon.caroline@gmail.com>
2020-10-31 22:00:26 +0100

    création de la classe Users

--- database/migrations/2020_10_31_204646_create_users_table.php
+++ database/migrations/2020_10_31_204646_create_users_table.php
@@ -14,7 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('mot_de_passe');
             $table->timestamps();
         });
     }
